7612abdb8ebf49b841d4f28576b20a1beb5b6cab
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-11 17:19:26 -0400

    Write fetch all merchants test

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Merchants API" do
+    it "sends a list of merchants" do
+        create_list(:merchant, 3)
+
+        get '/api/v1/merchants'
+
+        expect(response).to be_succesful
+    end
+    
+end
