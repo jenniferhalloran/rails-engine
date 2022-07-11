3ad0e03ab85d04dca0024715b807bd586a8741c5
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-11 17:43:56 -0400

    Pass get all merchants spec and postman

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe "Merchants API" do
         create_list(:merchant, 3)
 
         get '/api/v1/merchants'
-
+        
         expect(response).to be_successful
+    
+        merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(merchants.count).to eq(3)
+                
+        merchants.each do |merchant|
+            expect(merchant[:attributes]).to include(:id, :name)
+            expect(merchant[:attributes][:id]).to be_an(Integer)
+            expect(merchant[:attributes][:name]).to be_a(String)
+        end
     end
     
 end
