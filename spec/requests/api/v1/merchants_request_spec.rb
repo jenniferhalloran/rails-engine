53d72b5bed9fb5fd8d9b3093ce655d91053cf641
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-11 17:54:00 -0400

    Write test for individual merchant endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,5 +18,15 @@ RSpec.describe "Merchants API" do
             expect(merchant[:attributes][:name]).to be_a(String)
         end
     end
+
+    it "can get one merchant by its id" do
+        id = create(:merchant).id 
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(response).to be_successful
+    end
+    
     
 end
