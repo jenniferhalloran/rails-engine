044464bd323a12ff7e710a747ca3d65329f5dc6e
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-11 21:15:29 -0400

    Pass spec and postman tests for create an item endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: %i[index show new]
+      resources :items, only: %i[index show create]
       resources :merchants, only: %i[index show] do
         resources :items, only: %i[index], controller: :merchant_items
       end
